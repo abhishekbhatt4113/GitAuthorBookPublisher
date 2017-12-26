c9d0cace61096a8154e0005314cee95d698dc259
Abhishek Bhatt <abhishekbhatt4113@gmail.com>
2017-12-26 16:38:45 -0500

    Initial project copied from spring initiallizer

---
+++ src/main/java/com/git/authorBook/GitAuthorBookPublisher/GitAuthorBookPublisherApplication.java
@@ -0,0 +1,12 @@
+package com.git.authorBook.GitAuthorBookPublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAuthorBookPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitAuthorBookPublisherApplication.class, args);
+	}
+}
---
+++ src/test/java/com/git/authorBook/GitAuthorBookPublisher/GitAuthorBookPublisherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.git.authorBook.GitAuthorBookPublisher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitAuthorBookPublisherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
